c7b9827618932bf33b6257306732ee01db4d6f75
dwalero <49545973+dwalero@users.noreply.github.com>
2024-05-06 15:00:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "MealzCore",
-            path: "Sources/mealzcore.xcframework"
+            url: "https://github.com/Dwalero-org/MealzCoreRelease/releases/download/1.0.2/mealzcore.zip"
         )
     ]
 )
