352b3686e69bab9696fadf8c592df500efe4b1b6
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-05-06 17:35:03 +0200

    update release

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "MealzCore",
-            url: "https://github.com/Dwalero-org/MealzCoreRelease/releases/download/1.0.2/mealzcore.zip"
+            url: "https://github.com/Dwalero-org/MealzCoreRelease/blob/release/1.0.3/mealzcore.zip"
         )
     ]
 )
